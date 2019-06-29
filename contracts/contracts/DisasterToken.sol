bbbe1b73670f47e43dd773661213c19956e79145
Seita Uchimura <c201552k@gmail.com>
2019-06-30 03:09:59 +0900

    migration fileを追加

---
+++ contracts/contracts/DisasterToken.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.5.0;
+
+import "installed_contracts/openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol";
+
+contract DisasterToken is ERC721Full {
+    constructor (string memory name, string memory symbol) public ERC721Full(name, symbol) {
+        // solhint-disable-previous-line no-empty-blocks
+    }
+}
--- contracts/migrations/2_deploy_contract.js
+++ contracts/migrations/2_deploy_contract.js
@@ -1,10 +1,7 @@
-//const WalletFactory = artifacts.require("./WalletFactory.sol");
-//const ForwarderFactory = artifacts.require("./ForwarderFactory.sol");
+const DisasterToken = artifacts.require("./DisasterToken.sol");
 
 module.exports = async (deployer, network, accounts) => {
-    //deployer.deploy(MultiSig2of3, accounts[0], accounts[1], accounts[2]);
-    //deployer.then(async () => {
-        //await deployer.deploy(WalletFactory);
-        //await deployer.deploy(ForwarderFactory);
-    //})
+    const name = "DisasterToken";
+    const symbol = "DIT";
+    deployer.deploy(DisasterToken, name, symbol);
 };
