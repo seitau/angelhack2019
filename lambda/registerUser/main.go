fc189f39b440028fb51167ad7a1eea4e7e395373
nandehu0323 <hiroki.nanjo@ginco.io>
2019-06-30 03:17:53 +0900

    :wrench: Fix: registerUser

--- lambda/registerUser/main.go
+++ lambda/registerUser/main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-
-	"github.com/aws/aws-lambda-go/events"
+	"log"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+
+	"github.com/aws/aws-lambda-go/events"
 
 	"github.com/aws/aws-lambda-go/lambda"
 )
@@ -26,12 +27,20 @@ type Body struct {
 }
 
 // HandleLambdaEvent ...
-func HandleLambdaEvent(r Request) (events.APIGatewayProxyResponse, error) {
-	fmt.Println("Function Start")
-	if r.Name == "" || r.EthAddress == "" || r.AdaAddress == "" {
+func HandleLambdaEvent(r events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	var req Request
+	err := json.Unmarshal([]byte(r.Body), &req)
+	if err != nil {
+		log.Println(err)
+		jsonBytes, _ := json.Marshal(Body{Result: "JSON Unmarshal failed."})
+		return events.APIGatewayProxyResponse{StatusCode: 200, Headers: map[string]string{}, Body: string(jsonBytes)}, nil
+	}
+
+	if req.Name == "" || req.EthAddress == "" || req.AdaAddress == "" {
 		jsonBytes, _ := json.Marshal(Body{Result: "No data input."})
 		return events.APIGatewayProxyResponse{StatusCode: 200, Headers: map[string]string{}, Body: string(jsonBytes)}, nil
 	}
+
 	sess, err := session.NewSession()
 	if err != nil {
 		panic(err)
@@ -39,42 +48,52 @@ func HandleLambdaEvent(r Request) (events.APIGatewayProxyResponse, error) {
 
 	svc := dynamodb.New(sess)
 
-	getParams := &dynamodb.GetItemInput{
-		TableName: aws.String("Users-test"),
-		Key: map[string]*dynamodb.AttributeValue{
-			"username": {
-				S: aws.String(r.Name),
+	fmt.Println(req)
+
+	queryParams := &dynamodb.QueryInput{
+		TableName: aws.String("User-test"),
+		ExpressionAttributeNames: map[string]*string{
+			"#UserName": aws.String("username"),
+		},
+		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+			":username": {
+				S: aws.String(req.Name),
 			},
 		},
+		KeyConditionExpression: aws.String("#UserName=:username"),
+		IndexName:              aws.String("username-index"),
 	}
 
-	getItem, getErr := svc.GetItem(getParams)
-	if getErr == nil {
+	queryItem, queryErr := svc.Query(queryParams)
+	if queryErr == nil {
 		jsonBytes, _ := json.Marshal(Body{Result: "Username already used."})
 		return events.APIGatewayProxyResponse{StatusCode: 200, Headers: map[string]string{}, Body: string(jsonBytes)}, nil
 	}
-	fmt.Println(getItem)
+	log.Println(queryErr)
+	fmt.Println(queryItem)
 
 	putParams := &dynamodb.PutItemInput{
-		TableName: aws.String("Users-test"),
+		TableName: aws.String("User-test"),
 		Item: map[string]*dynamodb.AttributeValue{
 			"ethAddress": {
-				S: aws.String(r.EthAddress),
+				S: aws.String(req.EthAddress),
 			},
 			"adaAddress": {
-				S: aws.String(r.AdaAddress),
+				S: aws.String(req.AdaAddress),
 			},
 			"username": {
-				S: aws.String(r.Name),
+				S: aws.String(req.Name),
 			},
 		},
 	}
 
 	_, putErr := svc.PutItem(putParams)
 	if putErr != nil {
+		log.Println(putErr)
 		jsonBytes, _ := json.Marshal(Body{Result: "failed"})
 		return events.APIGatewayProxyResponse{StatusCode: 200, Headers: map[string]string{}, Body: string(jsonBytes)}, nil
 	}
+
 	jsonBytes, _ := json.Marshal(Body{Result: "success"})
 	return events.APIGatewayProxyResponse{StatusCode: 200, Headers: map[string]string{}, Body: string(jsonBytes)}, nil
 }
