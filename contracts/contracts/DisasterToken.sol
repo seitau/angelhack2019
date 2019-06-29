1c15d063f2b35dbdf009749b87eed05ab1dea56e
Seita Uchimura <c201552k@gmail.com>
2019-06-30 03:15:03 +0900

    successfully compiled

--- contracts/contracts/DisasterToken.sol
+++ contracts/contracts/DisasterToken.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.5.0;
 
-import "installed_contracts/openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol";
+import "../installed_contracts/openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol";
 
 contract DisasterToken is ERC721Full {
     constructor (string memory name, string memory symbol) public ERC721Full(name, symbol) {
