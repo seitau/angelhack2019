5d7ddb2cca81a913ad29fdc40e19fa1c5b221830
Seita Uchimura <c201552k@gmail.com>
2019-06-30 03:02:18 +0900

    openzeppelinをinstall

---
+++ contracts/migrations/2_deploy_contract.js
@@ -0,0 +1,10 @@
+//const WalletFactory = artifacts.require("./WalletFactory.sol");
+//const ForwarderFactory = artifacts.require("./ForwarderFactory.sol");
+
+module.exports = async (deployer, network, accounts) => {
+    //deployer.deploy(MultiSig2of3, accounts[0], accounts[1], accounts[2]);
+    //deployer.then(async () => {
+        //await deployer.deploy(WalletFactory);
+        //await deployer.deploy(ForwarderFactory);
+    //})
+};
