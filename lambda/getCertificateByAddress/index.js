c795e44c10d079da3944cd3bd2f343a16ae9ea55
Seita Uchimura <c201552k@gmail.com>
2019-06-30 09:22:06 +0900

    :wrench: Fix:関数名をgetCertificateByAddressに変更

---
+++ lambda/getCertificateByAddress/index.js
@@ -0,0 +1,49 @@
+const infuraEndpoint = "https://ropsten.infura.io/v3" + process.env.INFURA_ACCESS_TOKEN;
+const disasterTokenContractAddress = "0xdc7414410f683472553316Dadcb2c8763e07De8D";
+const Web3 = require('web3');
+const web3 = new Web3(infuraEndpoint);
+const balanceOfMethodId = "0x70a08231";
+const getCertificateMethodId = "0x70a08231";
+
+exports.handler = async (event, context, callback) => {
+    const body = event.body;
+    console.log(body.address);
+    if(!body.hasOwnProperty("address")) {
+        console.error('Address property must be specified in the request');
+        const err = new Error('Address property must be specified in the request');
+        callback(err, null);
+    } 
+    try {
+        const balanceOfTxData = balanceOfMethodId + web3.eth.abi.encodeParameter('address', body.address).slice(2);
+        const balanceHex = await web3.eth.call({
+            to: disasterTokenContractAddress,
+            data: balanceOfTxData
+        });
+        const balance = web3.utils.hexToNumber(balanceHex);
+        console.log("balance: " + balance);
+
+        if(balance === 0) {
+            console.error('No certificate is issued to this address');
+            const err = new Error('No certificate is issued to this address');
+            callback(err, null);
+        }
+
+        let certificates = new Array();
+        for(let i = 0; i < balance-1; i++) {
+            const id = web3.utils.keccak256(body.address + (balance-i));
+            console.log("id: " + id);
+
+            const getCertificateTxData = getCertificateMethodId + web3.eth.abi.encodeParameter('uint256', id).slice(2);
+            const certificate = await web3.eth.call({
+                to: disasterTokenContractAddress,
+                data: getCertificateTxData
+            });
+            console.log("certificate: " + certificate);
+            certificates.push(certificates);
+        }
+        callback(null, certificates);
+    } catch(err) {
+        console.error(err);
+        callback(err, null);
+    }
+}
